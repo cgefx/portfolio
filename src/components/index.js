046e9545221732abc746eff975cde5a7613774cb
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-10-26 15:00:18 -0700

    export new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,31 @@
+import Tabs from "./Tabs";
 import AnimationContainer from "./AnimationContainer";
 import AnimatedHeading from "./AnimatedHeading";
 import BaffleText from "./BaffleText";
+import ContactForm from "./ContactForm";
 import Footer from "./Footer";
 import Glitch from "./Glitch";
 import Layout from "./Layout";
 import Navbar from "./Navbar";
 import PageRevealer from "./PageRevealer";
+import PortfolioItem from "./PortfolioItem";
+import PortfolioItemDesktop from "./PortfolioItemDesktop";
 import Progress from "./Progress";
 import RevealContent from "./RevealContent";
 
 export {
+	Tabs,
 	AnimatedHeading,
 	AnimationContainer,
 	BaffleText,
+	ContactForm,
 	Footer,
 	Glitch,
 	Layout,
 	Navbar,
 	PageRevealer,
+	PortfolioItem,
+	PortfolioItemDesktop,
 	Progress,
 	RevealContent
 };
