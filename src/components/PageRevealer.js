b959ed09746de7e3acb0bf6b3583c020317b8fb2
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-10-26 19:43:41 -0700

    move styles outside of render method

--- src/components/PageRevealer.js
+++ src/components/PageRevealer.js
@@ -3,6 +3,52 @@ import styled, { keyframes } from "styled-components";
 import BaffleText from "./BaffleText";
 import AnimationContainer from "./AnimationContainer";
 
+const RevealContainer = styled.div`
+	position: fixed;
+	z-index: 100;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	align-items: center;
+	justify-content: center;
+	background-color: #000;
+	opacity: 1;
+	visibility: visible;
+	overflow-x: hidden !important;
+	overflow-y: hidden !important;
+	font-size: 50px;
+	color: #fff;
+	text-transform: uppercase;
+	font-weight: 700;
+	@media (max-width: 500px) {
+		font-size: 30px;
+	}
+`;
+
+const RevealAnimation = keyframes`
+			0% {
+					transform: translate3d(100%, 0, 0);
+			}
+			35%, 65% {
+					transform: translate3d(0, 0, 0);
+			}
+			100% {
+					transform: translate3d(-100%, 0, 0);
+			}
+	`;
+const Reveal = styled.div`
+	position: fixed;
+	width: 100%;
+	pointer-events: none;
+	height: 100%;
+	background-color: #04e5e5;
+	transform: translateX(100%);
+	&.animate {
+		animation: ${RevealAnimation} 1.1s cubic-bezier(0.2, 1, 0.3, 1) forwards;
+	}
+`;
+
 class PageRevealer extends React.Component {
 	constructor(props) {
 		super(props);
@@ -44,51 +90,6 @@ class PageRevealer extends React.Component {
 	}
 
 	render() {
-		const RevealContainer = styled.div`
-			position: fixed;
-			z-index: 100;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-			align-items: center;
-			justify-content: center;
-			background-color: #000;
-			opacity: 1;
-			visibility: visible;
-			overflow-x: hidden !important;
-			overflow-y: hidden !important;
-			font-size: 50px;
-			color: #fff;
-			text-transform: uppercase;
-			font-weight: 700;
-			@media (max-width: 500px) {
-				font-size: 30px;
-			}
-		`;
-
-		const RevealAnimation = keyframes`
-            0% {
-                transform: translate3d(100%, 0, 0);
-            }
-            35%, 65% {
-                transform: translate3d(0, 0, 0);
-            }
-            100% {
-                transform: translate3d(-100%, 0, 0);
-            }
-        `;
-		const Reveal = styled.div`
-			position: fixed;
-			width: 100%;
-			pointer-events: none;
-			height: 100%;
-			background-color: #04e5e5;
-			transform: translateX(100%);
-			&.animate {
-				animation: ${RevealAnimation} 1.1s cubic-bezier(0.2, 1, 0.3, 1) forwards;
-			}
-		`;
 		return (
 			<RevealContainer
 				id="reveal_container"
