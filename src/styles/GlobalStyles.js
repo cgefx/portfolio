28a9ae4196363514af08455476005363c8c54869
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-10-27 16:09:31 -0700

    make gradient variable

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+		:root {
+		--gradient: linear-gradient(
+			120deg,
+			#04e5e5,
+			#f37055,
+			#ef4e7b,
+			#a166ab,
+			#5073b8,
+			#1098ad,
+			#07b39b,
+			#6fba82
+		);
+	}
   html,
 	body {
 		height: 100%;
