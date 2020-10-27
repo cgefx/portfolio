f9111d5e489561636350abead8ff1874437f52d5
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-10-27 16:12:33 -0700

    use gradient variables

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -3,6 +3,88 @@ import styled, { keyframes } from "styled-components";
 import { StaticQuery, graphql } from "gatsby";
 import Typewriter from "typewriter-effect";
 import { Glitch } from "../components";
+import { SubHeading } from "../styles/Typography";
+import { StyledLink, gradientAnimation } from "../styles";
+
+const Overlay = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	background-color: rgba(0, 0, 0, 0.7);
+`;
+
+const Heading = styled.div`
+	.glitch {
+		font-size: 125px;
+		line-height: 140px;
+		font-weight: 600;
+		color: #fff;
+		@media (max-width: 767px) {
+			font-size: 40px;
+			line-height: 50px;
+		}
+	}
+`;
+
+const Type = styled.div`
+	font-size: 50px;
+	line-height: 50px;
+	color: #fff;
+	text-transform: uppercase;
+	margin-left: 6px;
+	@media (min-width: 768px) and (max-width: 1500px) {
+		font-size: 23px;
+		line-height: 20px;
+	}
+	@media (max-width: 767px) {
+		font-size: 20px;
+		line-height: 20px;
+	}
+	span {
+		font-family: Teko;
+	}
+`;
+
+const HeadingBox = styled.div`
+	height: 500px;
+	width: 900px;
+	margin: auto;
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	position: relative;
+	&:after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		border-radius: 5px;
+		background: var(--gradient);
+		background-size: 300% 300%;
+		clip-path: polygon(
+			0% 100%,
+			5px 100%,
+			5px 5px,
+			calc(100% - 5px) 5px,
+			calc(100% - 5px) calc(100% - 5px),
+			5px calc(100% - 5px),
+			5px 100%,
+			100% 100%,
+			100% 0%,
+			0% 0%
+		);
+	}
+	&.animate:after {
+		animation: ${gradientAnimation} 2s ease-in-out infinite;
+	}
+	@media (max-width: 767px) {
+		height: 350px;
+	}
+`;
 
 class Hero extends React.Component {
 	constructor(props) {
@@ -55,128 +137,6 @@ class Hero extends React.Component {
 			background-repeat: no-repeat;
 		`;
 
-		const Overlay = styled.div`
-			width: 100%;
-			display: flex;
-			align-items: center;
-			background-color: rgba(0, 0, 0, 0.7);
-		`;
-
-		const Heading = styled.div`
-			.glitch {
-				font-size: 125px;
-				line-height: 140px;
-				font-weight: 600;
-				color: #fff;
-				@media (max-width: 767px) {
-					font-size: 40px;
-					line-height: 50px;
-				}
-			}
-		`;
-
-		const SubHeading = styled.h2`
-			font-size: 18px;
-			font-weight: 300;
-			color: #ccc;
-			text-transform: uppercase;
-			letter-spacing: 4px;
-		`;
-		const Type = styled.div`
-			font-size: 50px;
-			line-height: 50px;
-			color: #fff;
-			text-transform: uppercase;
-			margin-left: 6px;
-			@media (min-width: 768px) and (max-width: 1500px) {
-				font-size: 23px;
-				line-height: 20px;
-			}
-			@media (max-width: 767px) {
-				font-size: 20px;
-				line-height: 20px;
-			}
-			span {
-				font-family: Teko;
-			}
-		`;
-		const gradientAnimation = keyframes`
-            0% {
-              background-position: 15% 0%;
-            }
-            50% {
-              background-position: 85% 100%;
-            }
-            100% {
-              background-position: 15% 0%;
-            }
-          `;
-
-		const HeadingBox = styled.div`
-			height: 500px;
-			width: 900px;
-			margin: auto;
-			position: relative;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			flex-direction: column;
-			position: relative;
-			&:after {
-				content: "";
-				position: absolute;
-				top: 0;
-				left: 0;
-				right: 0;
-				bottom: 0;
-				border-radius: 5px;
-				background: linear-gradient(
-					120deg,
-					#04e5e5,
-					#f37055,
-					#ef4e7b,
-					#a166ab,
-					#5073b8,
-					#1098ad,
-					#07b39b,
-					#6fba82
-				);
-				background-size: 300% 300%;
-				clip-path: polygon(
-					0% 100%,
-					10px 100%,
-					10px 10px,
-					calc(100% - 10px) 10px,
-					calc(100% - 10px) calc(100% - 10px),
-					10px calc(100% - 10px),
-					10px 100%,
-					100% 100%,
-					100% 0%,
-					0% 0%
-				);
-			}
-			&.animate:after {
-				animation: ${gradientAnimation} 2s ease-in-out infinite;
-			}
-			@media (max-width: 767px) {
-				height: 350px;
-			}
-		`;
-
-		const Resume = styled.a`
-			color: #fff;
-			text-decoration: none;
-			border-bottom: 2px solid;
-			font-weight: 300;
-			transition: 0.5s;
-			margin-top: 40px;
-			&:hover {
-				color: #fff;
-				text-decoration: none;
-				border-color: #04e5e5;
-			}
-		`;
-
 		return (
 			<Section id="home">
 				<Overlay
@@ -202,7 +162,7 @@ class Hero extends React.Component {
 								}}
 							/>
 						</Type>
-						<Resume href="./">Download Resume</Resume>
+						<StyledLink href="./">Download Resume</StyledLink>
 					</HeadingBox>
 					{this.shapes()}
 				</Overlay>
